fb39034b207d320efe6e8e0aaf335a34c3365b57
Pratik <srivastavapratik35@gmail.com>
2022-09-01 13:26:12 +0530

    Created the whole layout

---
+++ src/components/Videos.jsx
@@ -0,0 +1,55 @@
+// import React from "react";
+// import { Stack, Box } from "@mui/material";
+
+// import { ChannelCard, VideoCard } from "./";
+
+// const Videos = ({ videos }) => {
+//   // if(!videos?.length) return <Loader />;
+
+//   return (
+//     <Stack
+//       direction={"row"}
+//       flexWrap="wrap"
+//       justifyContent="start"
+//       alignItems="start"
+//       gap={2}
+//     >
+//       {videos.map((item, idx) => (
+//         <Box key={idx}>
+//           {item.id.videoId && <VideoCard video={item} />}
+//           {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+//         </Box>
+//       ))}
+//     </Stack>
+//   );
+// };
+
+// export default Videos;
+
+import React from "react";
+
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos }) => {
+  console.log(videos);
+  return (
+    <Stack
+      direction={"row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
