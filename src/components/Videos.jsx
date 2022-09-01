017a35fcec41fd797bd4b56a8913713f429526b4
Pratik <srivastavapratik35@gmail.com>
2022-09-01 14:10:54 +0530

    🚀🚀Let's go

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,36 +1,37 @@
-import React from "react";
-
-const Videos = ({ videos }) => {
-  console.log(videos);
-  return <div>Videos</div>;
-};
-
-export default Videos;
-
 // import React from "react";
-
-// import { Stack, Box } from "@mui/material";
-
-// import { ChannelCard, VideoCard } from "./";
 
 // const Videos = ({ videos }) => {
 //   console.log(videos);
-//   return (
-//     <Stack
-//       direction={"row"}
-//       flexWrap="wrap"
-//       justifyContent="start"
-//       alignItems="start"
-//       gap={2}
-//     >
-//       {videos.map((item, idx) => (
-//         <Box key={idx}>
-//           {item.id.videoId && <VideoCard video={item} />}
-//           {item.id.channelId && <ChannelCard channelDetail={item} />}
-//         </Box>
-//       ))}
-//     </Stack>
-//   );
+//   return <div>Videos</div>;
 // };
 
 // export default Videos;
+
+import React from "react";
+
+import { Stack, Box } from "@mui/material";
+
+import { ChannelCard, VideoCard } from "./";
+import Loader from "./Loader";
+
+const Videos = ({ videos }) => {
+  if (!videos?.length) return <Loader />;
+  return (
+    <Stack
+      direction={"row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
+        <Box key={idx}>
+          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+export default Videos;
